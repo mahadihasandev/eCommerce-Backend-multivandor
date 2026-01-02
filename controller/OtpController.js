09581df7ec16b5a2e9453bf9a4736065cd78758a
mahadihasandev <mayhaydihasan.com@gmail.com>
2026-01-02 08:50:25 +0600

    otp fix

--- controller/OtpController.js
+++ controller/OtpController.js
@@ -5,14 +5,11 @@ let otp=async (req,res)=>{
 
    let {otp,email}=req.body
 
-   
-   let existUser=await UserSchema.findOne({email:email})
-   
-   
+   let existUser=await UserSchema.findOne({email:email}) 
    if(existUser){
         if(!existUser.emailVerified&&existUser.otp==otp){          
             await UserSchema.findOneAndUpdate({email:email},{otp:"",emailVerified:true})
-             res.send("otp match") 
+             res.send({success:"otp match"}) 
         }else{
             res.send({error:"Please enter a valid otp"})                       
         }        
